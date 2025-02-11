1c9c903d7672206bd7b52a1c8ce5802e10f8f588
selestineadhyatma <selestine.atma@gmail.com>
2025-02-11 14:45:56 +0700

    check box

--- resources/views/regristation/index.blade.php
+++ resources/views/regristation/index.blade.php
@@ -11,49 +11,60 @@
 
     <!-- Main css -->
     <link rel="stylesheet" href="{{ url('colorlib-regform-12/css/style.css') }}">
+    
+    <style>
+        label {
+            font-weight: bold;
+            color: black;
+            display: block;
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
 <body>
 
     <div class="main">
-
         <div class="container">
             <form method="POST" class="appointment-form" id="appointment-form">
-                <h2>education appointment form</h2>
+                <h2>Formulir Pendaftaran Sekata</h2>
                 <div class="form-group-1">
-                <form action="#" method="POST">
-            <div class="form-group">
-                <label for="nama">Nama Lengkap</label>
-                <input type="text" id="nama" name="nama" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="telepon">No. Telepon</label>
-                <input type="tel" id="telepon" name="telepon" required>
-            </div>
-            <div class="form-group">
-                <label for="jenis_kelamin">Jenis Kelamin</label>
-                <select id="jenis_kelamin" name="jenis_kelamin">
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                </select>
+                    <div class="form-group">
+                        <label for="nama">Nama Lengkap</label> 
+                        <input type="text" id="nama" name="nama" required>
                     </div>
-                </div>
-                <div class="form-group-2">
-                    <h3>How would you like to bo located ?</h3>
-                    <div class="select-list">
-                        <select name="confirm_type" id="confirm_type">
-                            <option seleected value="">By phone</option>
-                            <option value="by_email">By email</option>
+                    <div class="form-group">
+                        <label for="jenis_kelamin">Jenis Kelamin</label>
+                        <select id="jenis_kelamin" name="jenis_kelamin">
+                        <option value="" disabled selected>Pilih jenis kelamin</option>
+                            <option value="Laki-laki">Laki-laki</option>
+                            <option value="Perempuan">Perempuan</option>
                         </select>
                     </div>
-                    <div class="select-list">
-                        <select name="hour_appointment" id="hour_appointment">
-                            <option seleected value="">Hours : 8am 10pm</option>
-                            <option value="9h-11h">Hours : 9am 11pm</option>
+                    <div class="form-group">
+                        <label for="alamat">Alamat</label> 
+                        <input type="text" id="alamat" name="alamat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="perusahaan">Nama Instansi/Perusahaan</label> 
+                        <input type="text" id="perusahaan" name="perusahaan" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="telepon">No Whatsapp</label>
+                        <input type="tel" id="telepon" name="telepon" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="skema">Skema Kompetensi yang dipilih:</label>
+                        <select id="skema" name="skema[]" multiple>
+                            <option value="asisteninstruktur">Asisten Instruktur</option>
+                            <option value="instruktur">Instruktur</option>
+                            <option value="instruktursenior">Instruktur Senior</option>
+                            <option value="mastertrainer">Master Trainer</option>
                         </select>
+                        <small>Tahan tombol Ctrl (Windows) atau Command (Mac) untuk memilih lebih dari satu.</small>
                     </div>
                 </div>
                 <div class="form-check">
@@ -65,11 +76,10 @@
                 </div>
             </form>
         </div>
-
     </div>
 
     <!-- JS -->
     <script src="{{ url('colorlib-regform-12/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{ url('colorlib-regform-12/js/main.js')}}"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 </html>
