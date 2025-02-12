c5a6fd9be260673dce2593adf55900682c78ff55
selestineadhyatma <selestine.atma@gmail.com>
2025-02-12 22:51:42 +0700

    index update

--- resources/views/regristation/index.blade.php
+++ resources/views/regristation/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulir Pendaftaran Sekata</title>
-
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
-<body class="bg-cover bg-center bg-no-repeat flex justify-center items-center min-h-screen" 
+<body class="bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center min-h-screen" 
       style="background-image: url('image/bg.jpg');">
 
     <div class="card w-full max-w-lg bg-white bg-opacity-80 backdrop-blur-md shadow-lg p-6 rounded-lg">
@@ -73,5 +73,34 @@
             <button type="submit" class="btn w-full bg-red-500 text-white shadow-lg hover:bg-red-600 hover:shadow-xl transition-shadow duration-300 py-2 px-4 text-sm rounded-lg">Daftar Sekarang</button>
         </form>
     </div>
+
+   <!-- FOOTER SOSIAL MEDIA -->
+<footer class="w-full text-center py-4 bg-red-900 text-white mb-0">
+    <div class="flex justify-center gap-4 mt-2">
+        <a href="https://www.facebook.com/profile.php?id=100094426865230&mibextid=ZbWKwL" target="_blank">
+            <img src="image/fb.png" alt="Facebook" class="w-10 h-10 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+        </a>
+        <a href="https://www.instagram.com/sekata_consulting" target="_blank">
+            <img src="image/ig.png" alt="Instagram" class="w-10 h-10 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+        </a>
+        <a href="https://wa.me/6287771107292" target="_blank">
+            <img src="image/wa.png" alt="WhatsApp" class="w-10 h-10 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+        </a>
+        <a href="https://www.linkedin.com" target="_blank">
+            <img src="image/yt.png" alt="YouTube" class="w-10 h-10 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+        </a>
+    </div>
+</footer>
+
+<!-- FOOTER INFORMASI KONTAK -->
+<footer class="w-full text-center bg-red-500 text-white mt-0">
+    <p class="text-sm">Hubungi kami melalui email:</p>
+    <p class="text-lg font-semibold">
+        <a href="mailto:sekataconsulting7292@gmail.com" class="text-blue-200 hover:underline">
+           sekataconsulting7292@gmail.com
+        </a>
+    </p>
+</footer>
+
 </body>
 </html>
