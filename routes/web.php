28ec3e668279a9877d5bffe7b6bf6ebd00e8c182
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-02-27 11:26:04 +0700

    enhance(web): update route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegristrationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Prefix untuk regristration
+Route::prefix('regristration')->group(function () {
+    Route::get('/', [RegristrationController::class, 'index'])->name('regristration.index');
+    Route::get('/create', [RegristrationController::class, 'create'])->name('regristration.create');
+    Route::post('/store', [RegristrationController::class, 'store'])->name('regristration.store');
 });
 
 require __DIR__.'/auth.php';
